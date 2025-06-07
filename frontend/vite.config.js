39f8144b632007c5ab4ee1eaefa06ba47681fe0d
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-06-07 08:58:28 +0000

    Add Netlify configuration, set Node version, and update build script

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true, // Ensure public directory is copied
+  },
 })
